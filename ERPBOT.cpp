48d938fa53b47476a3638ade0066b584aacc70d8
fallengod2 <111170659+fallengod2@users.noreply.github.com>
2022-08-13 02:30:41 -0400

    added erpbot

---
+++ ERPBOT.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+int main() {
+    int sex;
+    std::cout << "want to esecsx!?!?!(0/1): ";
+    std::cin >> sex;
+    if (sex == 1) {
+        std::cout << "erp!!!!!!!!";
+        _sleep(5000);
+    } 
+    else if (sex == 0) {
+        std::cout << "no Erp :c";
+        _sleep(5000);
+    } 
+}
